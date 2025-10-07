e4966358e24c86a49616416fdd0315bc67172677
LETI-122631 <adrls@iscte-iul.pt>
2025-10-07 16:34:02 +0100

    Implement EmailService for sending emails using Jakarta Mail API

---
+++ src/main/java/iscte/ista/todo/service/EmailService.java
@@ -0,0 +1,36 @@
+package iscte.ista.todo.service;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.*;
+import java.util.Properties;
+
+public class EmailService {
+
+    private static final String EMAIL_FROM = System.getenv("APP_EMAIL");
+    private static final String EMAIL_PASSWORD = System.getenv("APP_EMAIL_PASSWORD");
+
+    public static void sendEmail(String recipient, String subject, String body) throws MessagingException {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(EMAIL_FROM, EMAIL_PASSWORD);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(EMAIL_FROM));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+        message.setSubject(subject);
+        message.setText(body);
+
+        Transport.send(message);
+    }
+}
+
